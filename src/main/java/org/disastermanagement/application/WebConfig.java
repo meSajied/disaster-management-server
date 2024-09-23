4133e3b9960cf3186c9c2b2bcc6145234161883d
meSajied <sajiedshahyousuf@gmail.com>
2024-09-23 23:16:38 +0600

    fix: security

--- src/main/java/org/disastermanagement/application/WebConfig.java
+++ src/main/java/org/disastermanagement/application/WebConfig.java
@@ -9,9 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("*") // Adjust this to your frontend's URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow necessary methods
-                .allowCredentials(true); // Allow credentials if needed (e.g., cookies, authorization headers)
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
     }
 }
